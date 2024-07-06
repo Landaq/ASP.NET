fa05ef0dc1adfdb9714a95fe6785e036bbfed94a
cho <cwnv2002@naver.com>
2024-07-07 00:22:37 +0900

    11장, 12장 완료

---
+++ DevCounter/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.EnterpriseServices.CompensatingResourceManager;
+using System.Net;
+
+
+namespace DevCounter
+{
+    public class Global : System.Web.HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // [1] 사이트 통계 1/3
+            Application["CurrentVisit"] = 0; // 현재 사용자
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            // [2] 사이트 통계 2/3
+            Application.Lock();
+            Application["CurrentVisit"] =
+                   Convert.ToInt32(Application["CurrentVisit"]) + 1; // 현재 사용자
+            Application.UnLock();
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+            // [3] 사이트 통계 3/3
+            Application.Lock();
+            Application["CurrentVisit"] =
+                (int)Application["CurrenVisit"] - 1; // 현재 사용자
+            Application.UnLock();
+        }
+        protected void Application_End(object sender, EventArgs e)
+        {
+            // Empty
+        }
+
+    }
+}
---
+++ DevStateManagement/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DevStateManagement
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // 애플리케이션 시작 시 실행되는 코드
+            Application["NOW"] = DateTime.Now;
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            // 새 세션이 시작할 때 실행되는코드
+            Session["Now"] = DateTime.Now;
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+            // 새 세션이 끝날 때 실행되는 코드
+            // 참고: Web.Config에서
+            // sessionstate 모드가 InPorc로 설정되어 있는 경우에만
+            // Session_End 이벤트가 발생합니다.
+            // 세션 모드가 StateServer 또는 SQLserver로
+            // 설정되어있는 경우 이 이벤트가 발생하지 않습니다.
+        }
+        protected void Application_End(object sender, EventArgs e)
+        {
+            // 응용 프로그램이 종료될 때 실행되는 코드
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // 처리되지 않은 오류가 발생할 때 실행 되는 코드
+        }
+    }
+}
