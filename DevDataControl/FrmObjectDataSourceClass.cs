bb8baef20652cccc9090d829628caff64e838114
wonKeun.CHO <cwnv2002@naver.com>
2024-08-03 16:52:23 +0900

    DevDataControl chart 제외

---
+++ DevDataControl/FrmObjectDataSourceClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web;
+
+namespace DevDataControl
+{
+    public class FrmObjectDataSourceClass
+    {
+        public SqlDataReader GetMemos()
+        {
+            SqlConnection objCon = new SqlConnection();
+
+            objCon.ConnectionString = ConfigurationManager.ConnectionStrings["DevADONETConnectionString"].ConnectionString;
+            objCon.Open();
+
+            SqlCommand objCmd = new SqlCommand();
+            objCmd.Connection = objCon;
+            objCmd.CommandText = "ListMemo";
+            objCmd.CommandType = CommandType.StoredProcedure;
+
+            return objCmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+    }
+}
